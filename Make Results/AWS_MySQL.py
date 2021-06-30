cb78d04bd138816864ac503c492b807d51a81818
Jake Garth <jake.garth@students.mq.edu.au>
2021-06-30 11:18:11 +1000

    Domains now are inserted in to the database immediately

--- Make Results/AWS_MySQL.py
+++ Make Results/AWS_MySQL.py
@@ -11,6 +11,205 @@ import geoip2.webservice
 
 
 
+def connect_to_database():
+    print("Converting Data to Database")
+    print("connecting to online mysql")
+
+    #Login to database
+    ENDPOINT="database-2.cuzgntwsj1dy.us-east-2.rds.amazonaws.com"
+    PORT="3306"
+    USR="admin"
+    PW = "Cyberhub"
+    REGION="us-east-2a"
+    DBNAME="database-2"
+    db = pymysql.connect(host = ENDPOINT, user = USR, password = PW, use_unicode = True)
+    cursor = db.cursor()
+    # Enforce UTF-8 for the connection.
+    cursor.execute('SET NAMES utf8mb4')
+    cursor.execute("SET CHARACTER SET utf8mb4")
+    cursor.execute("SET character_set_connection=utf8mb4")
+
+    cursor.execute("select version()")
+    print("cursor: "+str(cursor))
+
+    data = cursor.fetchone()
+    print(str(data))
+
+    sql = '''use ISP'''
+    cursor.execute(sql)
+
+    sql = '''show tables'''
+    print(cursor.execute(sql))
+
+    tables = cursor.fetchall()
+
+    print(tables)
+    return {'db':db, 'cursor':cursor}
+
+def insert_ISP(db, cursor, isp_name, user_fullname, user_email, time_now):
+    #MAKE A ISP Object in the DB
+    user_ip_address = getIPAddress()
+    Download_and_Upload = speed_test()
+    lat_and_long = get_my_location_from_IP()
+    sql = '''
+    insert into ISP(isp_name, time_date, user_ip_address, Download_Speed, Upload_Speed, isp_name_speedtest, ping, public_ip_address, latitude, longitude, country, email, full_name) values('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')''' % (isp_name, time_now, user_ip_address,
+    Download_and_Upload.get('download'), Download_and_Upload.get('upload'), Download_and_Upload.get('isp_name'), Download_and_Upload.get('ping'), Download_and_Upload.get('client_ip'),lat_and_long.get('latitude') , lat_and_long.get('longitude'), lat_and_long.get('country'), user_fullname, user_email)
+    cursor.execute(sql)
+    db.commit()
+    print("INSERTED ISP OBJECT")
+
+def get_most_recent_ISP_id(cursor, isp_name, time_now):
+    #Retrieve the ISP that was just inserted
+    sql = '''
+    (SELECT id FROM ISP WHERE (isp_name = '%s' AND time_date = '%s'))''' % (isp_name, time_now)
+    cursor.execute(sql)
+    ispID = cursor.fetchone()[0]
+    print("ispID: "+str(ispID))
+    return ispID
+
+def insert_DNSs(db, cursor, ispID):
+    dns_ips = listOfDNSs()[1]
+    print("DNS IPS------------------------------------")
+    print(dns_ips)
+    #this takes care of adding all the DNS's to the database
+    for DNS in dns_ips:
+        dns_address = dns_ips.get(DNS)
+        dns_name = DNS
+        if dns_name == 'MyDNS':
+            dns_public = 0
+            default_dns = 1
+        else:
+            dns_public = 1
+            default_dns = 0
+        sql = '''
+        insert into DNS(dns_address, dns_name, dns_public, ispID, default_dns) values('%s', '%s', '%s', '%s', '%s')''' % (dns_address, dns_name, dns_public, ispID, default_dns)
+        cursor.execute(sql)
+        db.commit()
+
+def get_most_recent_DNS_ids(cursor):
+    dns_ips = listOfDNSs()[1]
+    print("dns_ips: "+str(dns_ips))
+    list_of_dns_ids_in_database = {}
+    for DNS in dns_ips:
+        print("DNS: "+str(DNS))
+        #Gets most recently added DNS servers
+        sql = '''
+        (SELECT id FROM DNS WHERE (dns_name = '%s'))''' % (DNS)
+        cursor.execute(sql)
+
+        DNS_IDs = cursor.fetchall()
+
+        print("DNS_IDs: "+str(DNS_IDs))
+        #Get most recent DNS Inserted, might need to change this to do some kind of lock, maybe need to select the one with the same ISP id?
+        DNS_ID = DNS_IDs[-1][0]
+
+        list_of_dns_ids_in_database[DNS] = DNS_ID
+
+        print("list_of_dns_ids_in_database: "+str(list_of_dns_ids_in_database))
+
+    return list_of_dns_ids_in_database
+
+def insert_domain(db, cursor, domain, dns_ips):
+    domain_name = domain.domain
+    response_code = domain.responseCode
+    Traceroute = ' '.join([str(addr) for addr in domain.Traceroute])
+    Number_of_Hops = domain.Hops_to_Domain
+    cloudflare_blockpage = 0
+    blockpage = 0
+    if domain.domainCloudFlareBlockPage == True:
+        cloudflare_blockpage = 1
+    if domain.domainBlockPage == True:
+        blockpage = 1
+    number_script_tags = domain.Number_of_Script_Tags
+    domain_html = db.escape(domain.domain_html.encode(encoding = "utf-8"))
+
+    print("DOMAINS DEFAULT DNS IPS-------------------------------------------------------JAKE HERE 2")
+    print(domain.ISP_DNS_IPS)
+
+    DNS_ID_List_In_Database = {}
+    for DNS in dns_ips:
+
+        #Gets most recently added DNS servers
+        sql = '''
+        (SELECT id FROM DNS WHERE (dns_name = '%s'))''' % (DNS)
+        cursor.execute(sql)
+
+        DNS_IDs = cursor.fetchall()
+        #Get most recent DNS Inserted, might need to change this to do some kind of lock, maybe need to select the one with the same ISP id?
+        DNS_ID = DNS_IDs[-1][0]
+
+        #Inserts the domains each associated with each DNS
+        sql = '''
+        insert into Domain(domain_name, response_code, Traceroute , Number_of_Hops, cloudflare_blockpage, blockpage, dnsID, number_of_script_tags, html_returned)
+        values('%s', '%s', '%s', '%s', '%s','%s', '%s', '%s', %s)''' % (domain_name, response_code, Traceroute, Number_of_Hops, cloudflare_blockpage, blockpage, DNS_ID, number_script_tags, domain_html)
+
+        cursor.execute(sql)
+        domainID = cursor.lastrowid
+        DNS_ID_List_In_Database[DNS] = domainID
+        db.commit()
+
+    return DNS_ID_List_In_Database
+
+
+
+def insert_IPs(db, cursor, domain, DNS_ID_List_In_Database):
+    #list of all IPs returned by all the DNSs
+    All_IPs_From_All_DNS = domain.Resolved_IPs
+    #2 is all DNS's in list
+    #ip_to_dns_dict = listOfDNSs()[2]
+    #3 is only default DNS and Google and Cloudlfare, i.e. public
+    ip_to_dns_dict = listOfDNSs()[3]
+
+
+
+    #This code inserts the IP requests in to the database
+    for dns_ip in All_IPs_From_All_DNS:
+        #gathers all the results for each DNS
+        dns_ip = dns_ip
+        dns_name = ip_to_dns_dict.get(dns_ip)
+
+        response_code_list = domain.Response_Code_Different_DNS_List().get(dns_name)
+        ip_blockpage_list = domain.IPBlockPageList().get(dns_name)
+        ip_cloudflare_blockpage_list = domain.IPCloudFlareBlockPageList().get(dns_name)
+        ip_html_list = domain.IPHTMLPageList().get(dns_name)
+        Number_of_Scripts_List = domain.Number_of_Scripts_Different_DNS_List.get(dns_name)
+
+        count_position_of_ip = 0
+        #iterates through all IP's for a given DNS
+
+
+        for ip in All_IPs_From_All_DNS.get(dns_ip):
+
+            #Gathers results for that IP
+            response_code = response_code_list[count_position_of_ip] #fix this
+            blockpage = ip_blockpage_list[count_position_of_ip]
+            cloudflare_blockpage = ip_cloudflare_blockpage_list[count_position_of_ip]
+            number_of_script_tags = Number_of_Scripts_List[count_position_of_ip]
+            html = ip_html_list[count_position_of_ip]
+
+            count_position_of_ip += 1
+
+            #Fixes some name changes
+            if dns_name == 'Google' or dns_name == 'Optus': #should fix this later, this is bad code design
+                dns_name += 'DNS'
+
+            if dns_name == 'AARC':
+                dns_name = 'AARNet'
+
+            domainID = DNS_ID_List_In_Database.get(dns_name)
+
+            #need to rmeove first and last charaacters
+            html_safe_for_MySQL = db.escape(html.encode(encoding = "utf-8"))
+            #Insert the IP address in to the datbase
+            sql = '''
+                insert into Request(address, DNS_DELETELATER, domainID, ip_response_code, ip_blockpage, ip_cloudflare_blockpage, ip_number_of_script_tags, html_returned)
+                values('%s', '%s', '%s', '%s', '%s', '%s', '%s', %s)''' % (ip, dns_name, domainID, response_code, blockpage, cloudflare_blockpage, number_of_script_tags, html_safe_for_MySQL)
+
+            print("Inserted :"+str(ip))
+            cursor.execute(sql)
+            db.commit()
+
+
 def convert_domain_to_database(list_of_domain_objects, isp_name, user_fullname, user_email):
     print("Converting Data to Database")
     print("connecting to online mysql")
@@ -89,7 +288,7 @@ def convert_domain_to_database(list_of_domain_objects, isp_name, user_fullname, 
         cursor.execute(sql)
         db.commit()
 
-    #Adds 5 instances of the domain to the databse, one for each DNS
+    #Adds 3 instances of the domain to the databse, one for each DNS
     for domain in list_of_domain_objects:
 
         domain_name = domain.domain
@@ -123,8 +322,6 @@ def convert_domain_to_database(list_of_domain_objects, isp_name, user_fullname, 
             DNS_ID = DNS_IDs[-1][0]
 
             #Inserts the domains each associated with each DNS
-
-
             sql = '''
             insert into Domain(domain_name, response_code, Traceroute , Number_of_Hops, cloudflare_blockpage, blockpage, dnsID, number_of_script_tags, html_returned)
             values('%s', '%s', '%s', '%s', '%s','%s', '%s', '%s', %s)''' % (domain_name, response_code, Traceroute, Number_of_Hops, cloudflare_blockpage, blockpage, DNS_ID, number_script_tags, domain_html)
@@ -136,16 +333,11 @@ def convert_domain_to_database(list_of_domain_objects, isp_name, user_fullname, 
 
 
         #list of all IPs returned by all the DNSs
-        print("domain.Resolved_IPs")
-        print(domain.Resolved_IPs)
         All_IPs_From_All_DNS = domain.Resolved_IPs
         #2 is all DNS's in list
         #ip_to_dns_dict = listOfDNSs()[2]
         #3 is only default DNS and Google and Cloudlfare, i.e. public
         ip_to_dns_dict = listOfDNSs()[3]
-
-        print("All_IPs_From_All_DNS---------------")
-        print(All_IPs_From_All_DNS)
 
 
 
@@ -161,28 +353,12 @@ def convert_domain_to_database(list_of_domain_objects, isp_name, user_fullname, 
             ip_html_list = domain.IPHTMLPageList().get(dns_name)
             Number_of_Scripts_List = domain.Number_of_Scripts_Different_DNS_List.get(dns_name)
 
-            print("All_IPs_From_All_DNS")
-            print(All_IPs_From_All_DNS)
-            print("response_code_list")
-            print(response_code_list)
-            print("ip_blockpage_list")
-            print(ip_blockpage_list)
-
-            print("DNS NAME")
-            print(dns_name)
-
-
             count_position_of_ip = 0
             #iterates through all IP's for a given DNS
 
-            print("List of all IPS")
-            print(str(len(All_IPs_From_All_DNS.get(dns_ip))))
-            print(str(All_IPs_From_All_DNS.get(dns_ip)))
+
             for ip in All_IPs_From_All_DNS.get(dns_ip):
-                print("dns_ip")
-                print(dns_ip)
-                print("Ip")
-                print(ip)
+
                 #Gathers results for that IP
                 response_code = response_code_list[count_position_of_ip] #fix this
                 blockpage = ip_blockpage_list[count_position_of_ip]
--- Make Results/main.py
+++ Make Results/main.py
@@ -1,7 +1,7 @@
 from website_functions import *
 from domain import *
 from CSV_Methods import *
-from AWS_MySQL import convert_domain_to_database
+from AWS_MySQL import *
 import geocoder
 
 
@@ -23,8 +23,31 @@ def CalculateListOfDomains(openFile):
     user_ISP = input("What ISP are you using?")
     user_fullname = input("What is your name?")
     user_email = input("What is your email? ")
+    time_now = time.strftime('%Y-%m-%d %H:%M:%S')
+
+    #connect to database here
+    connection_dictionary = connect_to_database()
+    db = connection_dictionary.get('db')
+    cursor = connection_dictionary.get('cursor')
+    #Make and insert ISP here
+    insert_ISP(db = db, cursor = cursor, isp_name = user_ISP, user_fullname = user_fullname, user_email = user_email, time_now = time_now)
+    #get most recently insert ISP id
+    ispID = get_most_recent_ISP_id(cursor = cursor, isp_name = user_ISP, time_now = time_now)
+
+    print(ispID)
+    #Make and Insert DNS servers here
+    insert_DNSs(db = db, cursor = cursor, ispID = ispID)
+
+    DNS_dict = get_most_recent_DNS_ids(cursor = cursor)
+    DNS_names = DNS_dict.keys()
+
+
+    #collect the dnsID's here, and don't change them, this will hopefully stop any concurrency issues
+
 
     #Iterates through the domains from 'openFile' and writes results to CSV
+
+
     websiteList = []
     with open(openFile) as fp:
         Lines = fp.readlines()
@@ -36,20 +59,29 @@ def CalculateListOfDomains(openFile):
 
     for item in websiteList:
         #Just calculating different representations of the domain name.
-        domain = item
-        domainStripped = stripDomainName(domain)
+        domain_string = item
+        domainStripped = stripDomainName(domain_string)
         WebsiteNOHttp = domainStripped.get('WebsiteNOHttp')
         WebsiteNOHttpNoSlash  = domainStripped.get('WebsiteNOHttpNoSlash')
         WebsiteNoHttpNoWWWNoSlash  = domainStripped.get('WebsiteNoHttpNoWWWNoSlash')
 
         #Makes the objects. Each object then calculates its results
-        obj = Domain(domain = domain,domainNoHTTP = WebsiteNOHttp,domainNoHTTPNoSlash = WebsiteNOHttpNoSlash, domainNoHTTPNoSlashNoWWW =  WebsiteNoHttpNoWWWNoSlash)
+        domain = Domain(domain = domain_string, domainNoHTTP = WebsiteNOHttp,domainNoHTTPNoSlash = WebsiteNOHttpNoSlash, domainNoHTTPNoSlashNoWWW =  WebsiteNoHttpNoWWWNoSlash)
+        DNS_ID_List_In_Database = insert_domain(db = db, cursor = cursor, domain = domain, dns_ips = DNS_names)
+        print(DNS_ID_List_In_Database)
+        insert_IPs(db = db, cursor = cursor, domain = domain, DNS_ID_List_In_Database = DNS_ID_List_In_Database)
+    db.close()
 
+
+    '''
         #Stores each object in the list_of_domain_objects
         list_of_domain_objects.append(obj)
+    '''
+        #instead of append to list, just insert to database
 
         #Writes domain object to AWS RDS
-    convert_domain_to_database(list_of_domain_objects = list_of_domain_objects, isp_name = user_ISP, user_fullname = user_fullname, user_email = user_email)
+
+    #convert_domain_to_database(list_of_domain_objects = list_of_domain_objects, isp_name = user_ISP, user_fullname = user_fullname, user_email = user_email)
 
 
 
